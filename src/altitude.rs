c3e5f7400268107df4bc423f62fdf1d291893a1c
JMA <jarcher@balglobal.com>
2019-07-29 19:29:21 -0500

    Named altitudes

--- src/altitude.rs
+++ src/altitude.rs
@@ -11,26 +11,37 @@ impl FromStr for Altitude {
     type Err = ParseAltitudeError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        // If altitude is given as a single value...
-        if let Ok(altitude) = s.parse() {
-            return Ok(Altitude {
-                ap: altitude,
-                pe: altitude,
-            });
+        const KEOSYNCHRONOUS_ALTITUDE: f64 = 2_863_330.0;
+        
+        match s.to_lowercase().as_ref() {
+            "keosynchronous" | "geosynchronous" | "geostationary" => Ok(Altitude {
+                ap: KEOSYNCHRONOUS_ALTITUDE,
+                pe: KEOSYNCHRONOUS_ALTITUDE,
+            }),
+
+            _ => {
+                // If altitude is given as a single value...
+                if let Ok(altitude) = s.parse() {
+                    return Ok(Altitude {
+                        ap: altitude,
+                        pe: altitude,
+                    });
+                }
+
+                let mut parts = s.trim().split('x');
+
+                // FIXME: this ignores additional altitude segments.
+                Ok(Altitude {
+                    ap: parts
+                        .next()
+                        .ok_or(ParseAltitudeError::MissingSegment)?
+                        .parse()?,
+                    pe: parts
+                        .next()
+                        .ok_or(ParseAltitudeError::MissingSegment)?
+                        .parse()?,
+                })
+            }
         }
-
-        let mut parts = s.trim().split('x');
-
-        // FIXME: this ignores additional altitude segments.
-        Ok(Altitude {
-            ap: parts
-                .next()
-                .ok_or(ParseAltitudeError::MissingSegment)?
-                .parse()?,
-            pe: parts
-                .next()
-                .ok_or(ParseAltitudeError::MissingSegment)?
-                .parse()?,
-        })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-// FIXME: Allow named altitudes like "geostationary"
 // FIXME: Print delta v requirements, if possible? (may require data I'm not getting right now)
 // FIXME: make this work for inverted ratios--3:2 as well as 2:3
 // FIXME: abstract out the parsing code instead of just repeating it for both altitude and ratio
@@ -12,20 +11,6 @@ mod ratio;
 type Result<T, E = error::BuildParametersError> = std::result::Result<T, E>;
 
 fn main() {
-    // const KEOSYNCHRONOUS_ALTITUDE: f64 = 2_863_330.0;
-
-    // let orbit = Orbit::new(
-    //     KEOSYNCHRONOUS_ALTITUDE,    // Altitude in meters
-    //     KEOSYNCHRONOUS_ALTITUDE,    // Altitude in meters
-    //     6.0,                        // Time in hours
-    //     KERBIN_RADIUS,
-    // );
-
-    // match orbit.resonant_periapsis(2.0 / 3.0) {
-    //     None => println!("Impossible"),
-    //     Some(pe) => println!("{:0.2}", pe),
-    // }
-
     match command::Command::from_args().build() {
         Err(e) => println!("{:?}", e),
         Ok((ratio, orbit)) => println!("{}", orbit.resonant_periapsis(ratio.resonance()).unwrap()),
